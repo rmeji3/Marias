55aa5c9cd87c64f3b9464506e3345d7a804cd15f
--replace-all <rmeji3@uic.edu>
2025-06-13 14:03:15 -0700

    added order now and also specials

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -6,6 +6,7 @@ const Location = ({locationRef }) => {
     <div ref ={locationRef} className="flex flex-col">
         <hr className="w-[350px] h-[500px] bg-[#EDEBE8] border-0 mx-auto my-4" />  
         <h1 className="text-2xl font-bold">Location</h1>
+        <img src={Map} alt="map" />
         <p>Location component content goes here.</p>
     </div>
   );
--- src/components/specials.jsx
+++ src/components/specials.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Specials() {
+function Specials({specialsRef}) {
     return (
-        <div className="flex flex-col">
-        <hr className="w-[350px] h-[1px] bg-[#EDEBE8] border-0 mx-auto my-4" />
-        <h1 className="text-2xl font-bold">Specials</h1>
-        <p>Specials component content goes here.</p>
+        <div ref={specialsRef} className="flex flex-col h-[500px]">
+            <hr className="w-[350px] h-[1px] bg-[#EDEBE8] border-0 mx-auto my-4" />
+            <h1 className="text-2xl font-bold">Specials</h1>
+            <p>Specials component content goes here.</p>
         </div>
     );
 }

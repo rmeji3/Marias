e03e1490330dbee23150c978fa0fdb44aca8e144
--replace-all <rmeji3@uic.edu>
2025-06-13 17:18:13 -0700

    location

--- src/components/specials.jsx
+++ src/components/specials.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function Specials({specialsRef}) {
     return (
         <div ref={specialsRef} className="flex flex-col h-[500px]">
-            <hr className="w-[350px] h-[1px] bg-[#EDEBE8] border-0 mx-auto my-4" />
             <h1 className="text-2xl font-bold">Specials</h1>
             <p>Specials component content goes here.</p>
         </div>

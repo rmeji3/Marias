0c8c24222121764fc556ac9520eb4d846f7760b0
Francisco <fsanc38@uic.edu>
2025-06-15 20:49:12 -0500

    working menu

--- src/components/menuNavbar.jsx
+++ src/components/menuNavbar.jsx
@@ -46,14 +46,18 @@ function MenuNavbar({tacoRef, soupRef , burritoRef, alacarteRef, breakfastRef,de
       >
         <ul className="flex flex-col space-y-6 text-lg font-semibold">
           <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
-          {[
-            "A la Carte", "Speciality Dinners", "Taco", "Soups", "Seafood", "Salads",
-            "Side Orders", "Drinks", "Desserts", "Breakfast", "Burritos", "Tamales"
-          ].map((item) => (
-            <li key={item}>
-              <a href="#" onClick={(e) => { scrollToSection(tacoRef)(e); setIsOpen(false); }}>{item}</a>
-            </li>
-          ))}
+          <li><a href="#" onClick={(e) => {scrollToSection(alacarteRef)(e);setIsOpen(false);}}>A la Carte</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(specialtyRef)(e);setIsOpen(false);}}>Speciality Dinners</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(tamalesRef)(e);setIsOpen(false);}}>Tamales</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(tacoRef)(e);setIsOpen(false);}}>Tacos</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(burritoRef)(e);setIsOpen(false);}}>Burritos</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(soupRef)(e);setIsOpen(false);}}>Soups</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(seafoodRef)(e);setIsOpen(false);}}>Seafood</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(saladsRef)(e);setIsOpen(false);}}>Salads</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(drinksRef)(e);setIsOpen(false);}}>Drinks</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(sidesRef)(e);setIsOpen(false);}}>Sides & Extras</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(dessertRef)(e);setIsOpen(false);}}>Desserts</a></li>
+          <li><a href="#" onClick={(e) => {scrollToSection(breakfastRef)(e);setIsOpen(false);}}>Breakfast</a></li>
         </ul>
       </div>
     </nav>

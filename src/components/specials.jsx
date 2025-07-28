3fafc428510b7ee04682308ecad03f7273c85aed
Francisco <fsanc38@uic.edu>
2025-07-27 19:35:02 -0500

    fixes

--- src/components/specials.jsx
+++ src/components/specials.jsx
@@ -3,8 +3,10 @@ import Carousel from "./carousel";
 
 function Specials({specialsRef}) {
     const specialsText = [
+
+        
         "$10 breakfast starting from 8am to 12am, Mon-Fri!",
-        "Taco Tuesday is here! Enjoy single tacos for $2.70!",
+        "Taco Tuesday is here! Enjoy single tacos for $2.75!" ,
         "Thursday: But one get one 50% off on all burritos!",
         "Tuesday and Thursday: Enjoy 50% off on all margaritas!",
         
@@ -12,6 +14,9 @@ function Specials({specialsRef}) {
     return (
         <div ref={specialsRef} className="flex flex-col py-5">
             <h1 className="text-2xl font-bold text-center md:text-3xl lg:text-4xl">Specials</h1>
+            <p className = "text-center font-bold p-3">  * Not all tacos and burritos included. Exclusions apply.
+
+ *</p>
             <div className="flex-1 overflow-hidden">
                 <Carousel text={specialsText}/>
             </div>
